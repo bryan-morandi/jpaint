8a768ab0c3478d000cf2877885b34bfd0f790d6b
Byte-of-Bryan <bryan.morandi@gmail.com>
2021-07-08 13:05:08 -0500

    In ShapeDrawer I added code to now draw ellipses and triangles. For the triangles I decided to draw equilateral triangles that face North, South, East, or West depending on mouse click and release. I will add more triangle types later if needed

--- src/main/java/view/Shapes/ShapeDrawer.java
+++ src/main/java/view/Shapes/ShapeDrawer.java
@@ -3,6 +3,8 @@ package view.Shapes;
 import view.interfaces.IShape;
 
 import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
 
 public class ShapeDrawer {
     private final Graphics2D g2D;
@@ -15,12 +17,42 @@ public class ShapeDrawer {
         for ( IShape shape : MasterShapeList.masterShapeList.getShapeList()) {
             switch (shape.getShapeType()) {
                 case RECTANGLE:
+                    Rectangle2D.Double rectangle;
+                    rectangle = new Rectangle2D.Double(shape.getX(), shape.getY(), shape.getWidth(), shape.getHeight());
                     g2D.setColor(shape.getPrimaryColor());
-                    g2D.fillRect(shape.getX(), shape.getY(), shape.getWidth(), shape.getHeight());
+                    g2D.fill(rectangle);
+                    break;
+                case ELLIPSE:
+                    Ellipse2D.Double ellipses;
+                    ellipses = new Ellipse2D.Double(shape.getX(),shape.getY(),shape.getWidth(), shape.getHeight());
+                    g2D.setColor(shape.getPrimaryColor());
+                    g2D.fill(ellipses);
                     break;
                 case TRIANGLE:
-                case ELLIPSE:
-                System.out.println("Shape not yet implemented");
+                    Polygon triangle;
+                    int nPoints = 3;
+                    int[] xPoints;
+                    int[] yPoints;
+                    //equilateral triangle pointing north
+                    if (shape.getPressedPoint().x < shape.getReleasedPoint().x && shape.getPressedPoint().y < shape.getReleasedPoint().y ) {
+                        xPoints = new int[] {(shape.getPressedPoint().x + shape.getReleasedPoint().x) / 2, shape.getPressedPoint().x, shape.getReleasedPoint().x };
+                        yPoints = new int[] {shape.getPressedPoint().y , shape.getReleasedPoint().y, shape.getReleasedPoint().y  };
+                        }
+                    //equilateral triangle pointing south
+                    else if (shape.getPressedPoint().x > shape.getReleasedPoint().x && shape.getPressedPoint().y < shape.getReleasedPoint().y ) {
+                        xPoints = new int[] {(shape.getPressedPoint().x + shape.getReleasedPoint().x) / 2, shape.getPressedPoint().x, shape.getReleasedPoint().x };
+                        yPoints = new int[] { shape.getReleasedPoint().y, shape.getPressedPoint().y, shape.getPressedPoint().y  };
+                    }
+                    //equilateral triangle pointing east/west depending on pressed and released point
+                    else /*(shape.getX() > shape.getReleasedPoint().x && shape.getY() > shape.getReleasedPoint().getY() ||
+                            shape.getX() < shape.getReleasedPoint().x && shape.getY() > shape.getReleasedPoint().getY())*/ {
+                        xPoints = new int[] {shape.getPressedPoint().x, shape.getReleasedPoint().x, shape.getReleasedPoint().x  };
+                        yPoints = new int[] {(shape.getPressedPoint().y + shape.getReleasedPoint().y) / 2, shape.getPressedPoint().y, shape.getReleasedPoint().y  };
+                        }
+                    triangle = new Polygon(xPoints, yPoints, nPoints);
+                    g2D.setColor(shape.getPrimaryColor());
+                    g2D.fill(triangle);
+                //System.out.println("Shape not yet implemented");
                     break;
             }
         }
